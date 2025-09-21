b988ccf91ada66aab51fb7aa93fbd9957b677aa9
Josimar Silva <josimaralves.ti@gmail.com>
2025-09-21 21:55:05 +0200

    algo(2025-09-21): Two Numbers
    notes: Two numbers challenge from LeetCode.
    Great introduction to Linked Lists.
    Reminded me of how I used to sum numbers on a notebook back on middle
    school.
    Ref.: https://leetcode.com/problems/add-two-numbers
    language: Kotlin

---
+++ algorithms/leetcode/kotlin/src/main/kotlin/com/josimarsilva/leetcode/TwoNumbers.kt
@@ -0,0 +1,58 @@
+package com.josimarsilva.leetcode
+
+/**
+ *
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Constraints:
+ *
+ *  - The number of nodes in each linked list is in the range [1, 100].
+ *  - 0 <= Node.val <= 9
+ *  - It is guaranteed that the list represents a number that does not have leading zeros.
+ *
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ *
+ * Ref.: https://leetcode.com/problems/add-two-numbers/
+ */
+class TwoNumbers {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        val dummyHead = ListNode(0)
+        var current = dummyHead
+
+        var parent1 = l1
+        var parent2 = l2
+        var carry = 0
+
+        while (parent1 != null || parent2 != null) {
+            val x = parent1?.`val` ?: 0
+            val y = parent2?.`val` ?: 0
+            val sum = x + y + carry
+
+            carry = sum / 10
+
+            current.next = ListNode(sum % 10)
+            current = current.next!!
+
+            parent1 = parent1?.next
+            parent2 = parent2?.next
+        }
+
+        if (carry > 0) {
+            current.next = ListNode(carry)
+        }
+
+        return dummyHead.next
+    }
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
---
+++ algorithms/leetcode/kotlin/src/test/kotlin/com/josimarsilva/leetcode/TwoNumbersTest.kt
@@ -0,0 +1,52 @@
+package com.josimarsilva.leetcode
+
+
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TwoNumbersTest {
+
+    @ParameterizedTest
+    @MethodSource("samples")
+    fun `should add two numbers and return the sum as a linked list`(l1: IntArray, l2: IntArray, expected: IntArray) {
+        val l1Node = arrayToListNode(l1)
+        val l2Node = arrayToListNode(l2)
+
+        val result = TwoNumbers().addTwoNumbers(l1Node, l2Node)
+
+        assertThat(asIntArray(result)).isEqualTo(expected)
+    }
+
+    private fun samples(): Stream<Arguments> = Stream.of(
+        Arguments.of(intArrayOf(2, 4, 3), intArrayOf(5, 6, 4), intArrayOf(7, 0, 8)),
+        Arguments.of(intArrayOf(0), intArrayOf(0), intArrayOf(0)),
+        Arguments.of(intArrayOf(9, 9, 9, 9, 9, 9, 9), intArrayOf(9, 9, 9, 9), intArrayOf(8, 9, 9, 9, 0, 0, 0, 1))
+    )
+
+    private fun asIntArray(listNode: ListNode?): IntArray {
+        val list = mutableListOf<Int>()
+        var currentNode = listNode
+        while (currentNode != null) {
+            list.add(currentNode.`val`)
+            currentNode = currentNode.next
+        }
+        return list.toIntArray()
+    }
+
+    private fun arrayToListNode(array: IntArray): ListNode? {
+        if (array.isEmpty()) return null
+        val head = ListNode(array[0])
+        var currentNode = head
+        for (i in 1 until array.size) {
+            val newNode = ListNode(array[i])
+            currentNode.next = newNode
+            currentNode = newNode
+        }
+        return head
+    }
+}
