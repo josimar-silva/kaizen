5b21074f203a5755db8e19d5d6a25498fe2d96b8
Josimar Silva <josimaralves.ti@gmail.com>
2025-10-24 15:25:57 +0200

    chore(algorithms): move test to correct package

--- algorithms/leetcode/kotlin/src/test/kotlin/com/josimarsilva/leetcode/easy/NeedleHaystackTest.kt
+++ algorithms/leetcode/kotlin/src/test/kotlin/com/josimarsilva/leetcode/easy/NeedleHaystackTest.kt
@@ -1,12 +1,10 @@
-package com.josimarsilva.leetcode.common;
+package com.josimarsilva.leetcode.easy
 
-import com.josimarsilva.leetcode.easy.strStrNaive
-import com.josimarsilva.leetcode.easy.strStrNative
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -15,8 +13,8 @@ class NeedleHaystackTest {
     @ParameterizedTest
     @MethodSource("samples")
     fun `should find a needle in the haystack`(haystack: String, needle: String, expectedLocation: Int) {
-        assertThat(strStrNaive(haystack, needle)).isEqualTo(expectedLocation)
-        assertThat(strStrNative(haystack, needle)).isEqualTo(expectedLocation)
+        Assertions.assertThat(strStrNaive(haystack, needle)).isEqualTo(expectedLocation)
+        Assertions.assertThat(strStrNative(haystack, needle)).isEqualTo(expectedLocation)
     }
 
     private fun samples(): Stream<Arguments> = Stream.of(
