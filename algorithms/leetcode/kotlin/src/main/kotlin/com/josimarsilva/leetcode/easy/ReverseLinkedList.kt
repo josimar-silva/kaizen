6aa5e19e1b6f63cb07e6114c8b68deac21d2f548
Josimar Silva <josimaralves.ti@gmail.com>
2025-11-07 21:29:45 +0100

    algo(2025-11-07): Reverse Linked List
    notes: ⏪⏪⏪⏪⏪
    Ref.: https://leetcode.com/problems/reverse-linked-list
    language: Kotlin

---
+++ algorithms/leetcode/kotlin/src/main/kotlin/com/josimarsilva/leetcode/easy/ReverseLinkedList.kt
@@ -0,0 +1,31 @@
+package com.josimarsilva.leetcode.easy
+
+import com.josimarsilva.leetcode.medium.ListNode
+
+/**
+ *
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Constraints:
+ * - The number of nodes in the list is the range [0, 5000].
+ * - -5000 <= Node.val <= 5000
+ *
+ * Ref.: https://leetcode.com/problems/reverse-linked-list
+ */
+//Time: O(N) Space: O(1)
+fun reverseList(head: ListNode?): ListNode? {
+    if (head == null || head.next == null) return head
+
+    var previousNode: ListNode? = null
+    var currentNode = head
+
+    while (currentNode != null) {
+        val nextNode = currentNode.next
+
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+    }
+
+    return previousNode
+}
---
+++ algorithms/leetcode/kotlin/src/test/kotlin/com/josimarsilva/leetcode/easy/ReverseLinkedListTest.kt
@@ -0,0 +1,27 @@
+package com.josimarsilva.leetcode.easy;
+
+import com.josimarsilva.leetcode.common.arrayToListNode
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ReverseLinkedListTest {
+
+    @ParameterizedTest
+    @MethodSource("samples")
+    fun `should reverse a given linked list`(head: IntArray, expectedResult: IntArray) {
+        assertThat(reverseList(arrayToListNode(head))).isEqualTo(arrayToListNode(expectedResult))
+    }
+
+    private fun samples(): Stream<Arguments> = Stream.of(
+        Arguments.of(intArrayOf(1,2,3,4,5), intArrayOf(5,4,3,2,1)),
+        Arguments.of(intArrayOf(1,2), intArrayOf(2,1)),
+        Arguments.of(intArrayOf(1), intArrayOf(1)),
+        Arguments.of(intArrayOf(), intArrayOf()),
+    )
+
+}
